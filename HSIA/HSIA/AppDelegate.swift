230ef4d9412cbc771688eaf0bbf1408db3ee5f04
Saffet Emin Reisoğlu <sereisoglu@gmail.com>
2020-02-27 15:35:04 +0300

    The project has been created.

---
+++ HSIA/HSIA/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  HSIA
+//
+//  Created by Saffet Emin Reisoğlu on 27.02.2020.
+//  Copyright © 2020 Saffet Emin Reisoğlu. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+}
---
+++ HSIA/HSIA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HSIA
+//
+//  Created by Saffet Emin Reisoğlu on 27.02.2020.
+//  Copyright © 2020 Saffet Emin Reisoğlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .red
+    }
+
+}
